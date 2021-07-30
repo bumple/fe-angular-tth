40c33ad70a3cd9719bed30e94539fd89e9cca2e6
TamNguyen <ngductam91@gmail.com>
2021-07-30 10:28:03 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,27 +5,38 @@ import {LoginComponent} from "./components/layouts/pages/login/login.component";
 import {RegisterComponent} from "./components/layouts/pages/register/register.component";
 import {WalletInfoComponent} from "./components/wallets/wallet-info/wallet-info.component";
 import {WalletCreateComponent} from "./components/wallets/wallet-create/wallet-create.component";
+import {CategoryListComponent} from "./components/categories/category-list/category-list.component";
 
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'login',
     component: LoginComponent,
   },
   {
-    path: 'wallet',
-    component:LayoutsComponent,
+    path: '',
+    component: LayoutsComponent,
     children: [
       {
-        path: 'info',
-        component: WalletInfoComponent
+        path: 'wallet',
+        children: [
+          {
+            path: 'info',
+            component: WalletInfoComponent
+          },
+          {
+            path: 'create',
+            component: WalletCreateComponent
+          }
+        ],
       },
       {
-        path: 'create',
-        component: WalletCreateComponent
-      }
+        path: 'category',
+        component: CategoryListComponent
+      },
     ]
   },
+
   {
     path: 'register',
     component: RegisterComponent
